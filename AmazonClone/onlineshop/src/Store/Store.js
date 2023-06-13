77935488d8ac86e7d22692b4c33857db078934ba
Gonza <fionagonza2@gmail.com>
2023-06-13 13:24:53 +0300

    Deleted the AmazonClone directory

--- AmazonClone/onlineshop/src/Store/Store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import ReducerFunction from '../Contexts/dispatchContext'
-
-export const store = configureStore({
- reducer:{
-  mainReducer:ReducerFunction
- }
-})
--- AmazonClone/onlineshop/src/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const instance = axios.create({
-	baseURL: "http://127.0.0.1:5001/e-commercesite-12cac/us-central1/api",
-});
-
-export default instance;
